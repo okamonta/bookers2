1c36fda56cb40f7058dbde39f78140ef554ff7c4
EC2 Default User <ec2-user@ip-172-31-6-163.us-east-2.compute.internal>
2021-09-30 02:14:23 +0000

    いいね機能、コメント機能完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,11 +4,11 @@ class BookCommentsController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    @comment = BookComment.new(book_comment_params)
-    @comment.user_id = current_user.id
-    @comment.book_id = @book.id
-    @comment_new = BookComment.new
-    if @comment.save
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    # @comment_new = BookComment.new
+    if @book_comment.save
       redirect_to book_path(@book.id)
     else
       @user = @book.user
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -2,5 +2,6 @@ class BookComment < ApplicationRecord
   belongs_to :book
   belongs_to :user
   
+  # 空のメッセージでエラーにならない。
   validates :comment, presence: true
 end
